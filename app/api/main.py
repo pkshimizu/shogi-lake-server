6a7fae2118382030fab33c8cc5a2e8a7b197cd15
shimizu kenji <shimizu.kenji@noncore.net>
2022-12-06 23:23:11 +0900

    feat: Blueprint Module設定

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 app = Flask(__name__)
+
+v1 = Blueprint("v1", __name__, url_prefix="/v1")
+
+app.register_blueprint(v1)
 
 
 @app.route("/")
