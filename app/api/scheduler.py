ab7cb0780e76179756da9cf54dd17107a7e7792c
shimizu kenji <shimizu.kenji@noncore.net>
2023-01-13 08:44:49 +0900

    feat: ニュース収集スケジュール追加

--- app/api/scheduler.py
+++ app/api/scheduler.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask_apscheduler import APScheduler
 
+from app.api.application import NewsCollectService, NewsFetchService
+
 scheduler = APScheduler()
 
 
 def setup_scheduler(app: Flask):
     scheduler.init_app(app)
 
-    @scheduler.task("interval", id="job_test", seconds=60, misfire_grace_time=900)
-    def job_test():
-        print("job test!!!")
+    @scheduler.task("cron", id="job_collect_news", minute="05")
+    def job_collect_news():
+        from app.api.injector import injector
+
+        news_collect_service = injector.get(NewsCollectService)
+        news_collect_service.collect_shogi_federation()
+        news_collect_service.collect_yomiuri_news()
+        news_collect_service.collect_asahi_news()
+        news_collect_service.collect_mainichi_news()
+        news_collect_service.collect_hokkaido_news()
+
+        news_fetch_service = injector.get(NewsFetchService)
+        news_fetch_service.fetch_image()
 
     scheduler.start()
